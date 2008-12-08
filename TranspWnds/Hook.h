d70a8f5d8954082972514f09882d670c210c8690
UncleLab <UncleLab@a5a91ff0-b971-11dd-940c-8b96d25ca2fe>
2008-12-08 21:42:29 +0000

    merge with 1.2.0

---
+++ TranspWnds/Hook.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <windows.h>
+#include <map>
+
+typedef struct tagHotKeyInfo
+{
+	bool IsHotKey(UINT uMsg,PMSLLHOOKSTRUCT lpMouseHookStruct);
+	BOOL m_fAlt;
+	BOOL m_fCtrl;
+	BOOL m_fShift;
+	BOOL m_fWin;
+	UINT m_uMsg;
+	tagHotKeyInfo& operator=(tagHotKeyInfo& HotKeyInfo);
+}HOTKEYINFO,*LPHOTKEYINFO;
+
+enum enHotKeyOperations
+{
+	hkoTransp,
+	hkoTopMost,
+	hkoCount
+};
+
+class CHook
+{
+public:
+	HHOOK m_hMouse;
+	///\brief структура, содержащая информацию об окне
+	///\param bAlpha - альфаканал
+	///\param fAlpha - TRUE,если окну был присвоен стиль WS_EX_LAYERED 
+	///\param fTopMost - TRUE,если окну был присвоен стиль WS_EX_TOPMOST 
+	typedef struct tagWndInfo
+	{
+		BYTE bAlpha;
+		BOOL fAlpha;
+		BOOL fTopMost;
+	}WNDINFO,*LPWNDINFO;
+	///\brief мап окно - информация о нём
+	std::map<HWND,WNDINFO> m_mapWndInfo;
+
+
+	HOTKEYINFO m_arHotKeyInfo[hkoCount];
+
+	BYTE m_bMinTranspVal;
+	BYTE m_bTranspStep;
+public:
+	CHook(void);
+	~CHook(void);
+
+	void Enable();
+	void Disable();
+	void Restore();
+
+	static CHook* GetHook();
+public:
+	LRESULT ProcessTransp(UINT uMsg, PMSLLHOOKSTRUCT lpMouseHookStruct);
+	LRESULT ProcessTopMost(UINT uMsg, PMSLLHOOKSTRUCT lpMouseHookStruct);
+};
