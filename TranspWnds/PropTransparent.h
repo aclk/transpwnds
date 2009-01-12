2530e3949edc6d38ebc1d8632c12d8dda7033f18
UncleLab <UncleLab@a5a91ff0-b971-11dd-940c-8b96d25ca2fe>
2009-01-12 21:36:20 +0000

    create 1.2.7

---
+++ TranspWnds/PropTransparent.h
@@ -0,0 +1,84 @@
+#pragma once
+#include "ULLibNS.h"
+
+//=======================================================
+namespace ULWnds
+{
+	namespace ULControls
+	{
+		///\class CULSlider
+		///\brief класс обёртка для slider control
+		class CULSlider:
+			public CULSubClass
+		{
+		public:
+			///\brief конструктор
+			CULSlider();
+			///\brief Функция для создания слайдера
+			///\param hParentWnd - хендл родительского окна
+			///\param uID - ID слайдера
+			///\param szCaption - заголовок слайдера
+			///\param x,y - координата слайдера
+			///\param cx,cy - размер слайдера
+			///\param dwStyle - стиль слайдера
+			///\param dwStyleEx - расширенный стиль 
+			///\return TRUE в случае успеха
+			BOOL Create(HWND hParentWnd,UINT uID,LPCTSTR szCaption,int x,int y,int cx,int cy,
+				DWORD dwStyle=WS_CHILD|WS_VISIBLE|TBS_AUTOTICKS,DWORD dwStyleEx=NULL);
+			///\brief функция задаёт диапазон для слайдера
+			///\param nMin - минимальноe значение
+			///\param nMax - максимальное значение
+			///\param fRedraw - флаг перерисовки слайдера при изменении диапазона
+			void SetRange(int nMin,int nMax,BOOL fRedraw=TRUE);
+			///\brief функция задаёт минимальное значение для слайдера
+			///\param nMin - минимальноe значение
+			///\param fRedraw - флаг перерисовки слайдера при изменении диапазона
+			void SetRangeMin(int nMin,BOOL fRedraw=TRUE);
+			///\brief функция задаёт максимальное значение для слайдера
+			///\param nMax - максимальное значение
+			///\param fRedraw - флаг перерисовки слайдера при изменении диапазона
+			void SetRangeMax(int nMax,BOOL fRedraw=TRUE);
+			///\brief устанавливает позицию бегунка слайдера
+			///\param nPos - позиция бегунка
+			///\param fRedraw - флаг перерисовки слайдера при позиции бегунка
+			void SetPos(int nPos,BOOL fRedraw=TRUE);
+			///\brief возвращает позицию бегунка слайдера
+			///\return позиция бегунка
+			int GetPos();
+			///\brief устанавливает длину бегунка
+			///\param nLen - длина бегунка
+			void SetThumbLenght(UINT nLen);
+			///\brief устанавливает частоту отрисовки маркеров
+			///\param nFreq - частота отрисовки маркеров
+			void SetTicFreq(UINT nFreq);
+			///\brief устанавливает шаг изменения позиции курсором клавиатуры
+			///\param nLineSize - шаг изменения позиции
+			///\return предыдущий шаг изменения позиции
+			UINT SetLineSize(UINT nLineSize);
+			///\brief устанавливает шаг изменения позиции кнопками PageUp/PageDown \n
+			/// и кликом мыши по свободной области
+			///\param nLineSize - шаг изменения позиции
+			///\return предыдущий шаг изменения позиции
+			UINT SetPageSize(UINT nPageSize);
+		};
+	}
+}
+//=======================================================
+
+
+class CPropTransparent :
+	public CULPropPage
+{
+protected:
+	CULSlider m_sliderMinTranspLevel;
+	CULSlider m_sliderTranspLevelStep;
+public:
+	int m_nMinTransparentLevel;
+	int m_nTransparentLevelStep;
+public:
+	CPropTransparent(void);
+	~CPropTransparent(void);
+protected:
+	virtual LRESULT OnInitDialog(WPARAM,LPARAM);
+	virtual LRESULT OnHScroll(WPARAM,LPARAM);
+};
