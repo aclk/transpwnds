38ea2ea86ee9b8854c006d16a52bcea174ce5beb
UncleLab <UncleLab@a5a91ff0-b971-11dd-940c-8b96d25ca2fe>
2009-02-08 05:12:01 +0000

    create tag 1.3.0

---
+++ tags/1.3.0/TranspWnds/resource.h
@@ -0,0 +1,116 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TranspWnds.rc
+//
+#define IDR_RT_MANIFEST1                1
+#define IDD_ABOUTBOX                    100
+#define IDI_ICON1                       101
+#define IDI_MAINICON                    101
+#define IDM_ABOUT                       101
+#define IDS_MENU_ENABLE                 102
+#define IDM_ENABLE                      102
+#define IDD_PROPPAGE_TRANSPARENT        102
+#define IDS_MENU_DISABLE                103
+#define IDM_DISABLE                     103
+#define IDD_VIEWING_WINDOWS             103
+#define IDS_MENU_RESTORE                104
+#define IDM_RESTORE                     104
+#define IDS_MENU_QUIT                   105
+#define IDM_AUTORUN                     105
+#define IDD_DIALOG_UPDATEPROGRESS       105
+#define IDS_APP_NAME                    106
+#define IDD_PROPPAGE_HOTKEYS            106
+#define IDM_OPTIONS                     106
+#define IDS_MENU_OPTIONS                107
+#define IDD_PROPPAGE_SYSTEM             107
+#define IDM_QUIT                        107
+#define IDS_OPTIONSDLG_CAPTION          108
+#define IDM_VIEWING                     108
+#define IDD_PROPPAGE_OSD                108
+#define IDS_OPTOINS_TOPMOST_COMBO_LBTN  109
+#define IDS_OPTOINS_COMBO_LBTN          109
+#define IDS_OPTOINS_TOPMOST_COMBO_RBTN  110
+#define IDS_OPTOINS_COMBO_RBTN          110
+#define IDS_OPTOINS_TOPMOST_COMBO_MBTN  111
+#define IDS_OPTOINS_COMBO_MBTN          111
+#define IDS_COMPANY_NAME                112
+#define IDS_MENU_ABOUT                  113
+#define IDS_MENU_VIEWING                114
+#define IDS_VIEWING_LIST_COLUMN_CAPTION 115
+#define IDS_VIEWING_LIST_COLUMN_TRANSPARENT 116
+#define IDS_VIEWING_LIST_COLUMN_TRANSPARENT_LEVEL 117
+#define IDS_VIEWING_LIST_COLUMN_TOPMOST 118
+#define IDS_VIEWING_LIST_COLUMN_USECAPTION 119
+#define IDS_VIEWING_LIST_COLUMN_HIDECAPTION 119
+#define IDS_CHECKFORUPDATE_CONNECT      120
+#define IDS_CHECKFORUPDATE_CURRENTVERSION 121
+#define IDS_CHECKFORUPDATE_OK           122
+#define IDS_CHECKFORUPDATE_FAIL         123
+#define IDS_CHECKFORUPDATE_RECEIVEDATA  124
+#define IDS_CHECKFORUPDATE_PARSEDATA    125
+#define IDS_CHECKFORUPDATE_LASTVERSION  126
+#define IDS_CHECKFORUPDATE_NEWVERSIONAVAIL 127
+#define IDS_CHECKFORUPDATE_ERROR        128
+#define IDS_CHECKFORUPDATE_DATANAME     129
+#define IDS_DOWNLOADPAGE                130
+#define IDS_DOWNLOAD_NOTIFY             131
+#define IDS_DOWNLOAD_NOTIFY_CAPTION     132
+#define IDC_CHECK_TRANSP_ALT            1001
+#define IDC_CHECK_TRANSP_CTRL           1002
+#define IDC_CHECK_TRANSP_SHIFT          1003
+#define IDC_CHECK_TRANSP_WIN            1004
+#define IDC_CHECK_TOPMOST_ALT           1005
+#define IDC_CHECK_TOPMOST_CTRL          1006
+#define IDC_CHECK_TOPMOST_SHIFT         1007
+#define IDC_CHECK_TOPMOST_WIN           1008
+#define IDC_COMBO_TOPMOST_MSG           1009
+#define IDC_COMBO_MOVE_MSG              1010
+#define IDC_CHECK2                      1011
+#define IDC_CHECK_AUTORUN               1011
+#define IDC_CHECK_MOVE_ALT              1011
+#define IDC_CHECK_MOVE_CTRL             1012
+#define IDC_CHECK_MOVE_SHIFT            1013
+#define IDC_SLIDER_MIN_TRANSP_LEVEL     1013
+#define IDC_CHECK_MOVE_WIN              1014
+#define IDC_SLIDER_TRANSP_LEVEL_STEP    1014
+#define IDC_LIST_VIEWING_WINDOWS        1014
+#define IDC_COMBO_SIZE_MSG              1015
+#define IDC_RADIO_TOPLEFT               1015
+#define IDC_CHECK_SIZE_ALT              1016
+#define IDC_RADIO_TOPRIGHT              1016
+#define IDC_CHECK_SIZE_CTRL             1017
+#define IDC_RADIO_CENTER                1017
+#define IDC_CHECK_SIZE_SHIFT            1018
+#define IDC_RADIO_BOTLEFT               1018
+#define IDC_CHECK_SIZE_WIN              1019
+#define IDC_RADIO5                      1019
+#define IDC_RADIO_BOTRIGHT              1019
+#define IDC_COMBO_TOGGLECAPTION_MSG     1020
+#define IDC_BUTTON_FONT                 1020
+#define IDC_CHECK_TOGGLECAPTION_ALT     1021
+#define IDC_BUTTON_TEXTCOLOR            1021
+#define IDC_CHECK_TOGGLECAPTION_CTRL    1022
+#define IDC_BUTTON_TEXTSHADOWCOLOR      1022
+#define IDC_CHECK_TOGGLECAPTION_SHIFT   1023
+#define IDC_STATIC_TEXTCOLOR            1023
+#define IDC_CHECK_TOPMOST_WIN2          1024
+#define IDC_CHECK_TOGGLECAPTION_WIN     1024
+#define IDC_STATIC_TEXTSHADOWCOLOR      1024
+#define IDC_RADIO_ATSTART               1025
+#define IDC_RADIO_EVERY24HOURS          1026
+#define IDC_RADIO_ATSTARTANDEVERY24HOURS 1027
+#define IDC_BUTTON_CHECKNOW             1028
+#define IDC_RADIO_NEVER                 1029
+#define IDC_LIST_UPDATELOG              1031
+#define IDC_STATIC_DOWNLOADPAGE         1032
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1033
+#define _APS_NEXT_SYMED_VALUE           109
+#endif
+#endif
